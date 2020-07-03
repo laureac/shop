29c4eabe5fc6372b4aa096587f7155a37ee5e220
Laure Claret <laure.a.claret@gmail.com>
2020-07-03 13:31:00 +1000

    Updates

---
+++ precache-manifest.bba21567b6c333cd9d41cb4a16411753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25bbdb48bf4059958fb1d38ae99d198d",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "525cb7eb6e2ae700d432",
+    "url": "/shop/static/css/main.6b2aeb04.chunk.css"
+  },
+  {
+    "revision": "242eff8fcc5f27a94cf1",
+    "url": "/shop/static/js/2.9f1dfe1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shop/static/js/2.9f1dfe1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "525cb7eb6e2ae700d432",
+    "url": "/shop/static/js/main.ba361e95.chunk.js"
+  },
+  {
+    "revision": "28b68c8753cb905abfe2",
+    "url": "/shop/static/js/runtime-main.72ff19cf.js"
+  }
+]);
